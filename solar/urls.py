ed5b9ca0040f888a46ac5731aa9f4d629b4cc3e7
agrim <agrim.sharma@sirez.com>
2018-04-19 16:12:11 +0530

    upated

--- solar/urls.py
+++ solar/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('add/', add_document, name='add_document'),
     path('search/', search_document, name='search_document'),
     path('search_city/', search_city_document, name='search_city_document'),
+    path('university/', add_university, name='add_university'),
+    path('property/', add_property, name='add_property'),
+    path('city/', add_city, name='add_city'),
 ]
--- solar/views.py
+++ solar/views.py
@@ -17,6 +17,7 @@ def fetch_data(url):
     response = json.loads(response)['response']
     if response['numFound'] > 0:
         return response
+    else: return ""
 
 
 @csrf_exempt
@@ -103,15 +104,130 @@ def search_city_document(request):
         else:
             university = fetch_data("/solr/university/select?wt=json&indent=true&q=city:{}*&fl=name,city".format(city[:3]))
             propertys = fetch_data("/solr/property/select?wt=json&indent=true&q=city:{}*&fl=Address,city".format(city[:3]))
-
-            return HttpResponse(
-                json.dumps(
+            if university == "" and propertys == "":
+                response = json.dumps(
+                    dict(
+                        city=city,
+                        university="No records found",
+                        property="No records found"))
+            elif university["numFound"] > 0 and propertys['numFound'] > 0:
+                response = json.dumps(
+                        dict(
+                            city=city,
+                            university=[dict(name=u['name'][0], city=u['city'][0]) for u in university['docs']] ,
+                            property=[dict(address=u['Address'][0], city=u['city'][0]) for u in propertys['docs']]))
+            elif university['numFound'] > 0 and propertys['numFound'] == 0:
+                response = json.dumps(
                     dict(
                         city=city,
                         university=[dict(name=u['name'][0], city=u['city'][0]) for u in university['docs']],
-                        property=[dict(address=u['Address'][0], city=u['city'][0]) for u in propertys['docs']])))
+                        property="No records found"))
+            elif university['numFound'] == 0 and propertys['numFound'] > 0:
+                response = json.dumps(
+                    dict(
+                        city=city,
+                        university="No records found",
+                        property=[dict(address=u['Address'][0], city=u['city'][0]) for u in propertys['docs']]))
+            else:
+                response = json.dumps(
+                    dict(
+                        city=city,
+                        university="No records found",
+                        property="No records found"))
+            return HttpResponse(response)
     else:
-        return render(request, "search_city_document.html")
+        city = fetch_data("/solr/city/select?wt=json&indent=true&q=*:*")
+        if city['numFound'] > 0:
+            city = city['docs']
+
+            return render(request, "search_city_document.html", dict(city=[c['name'][0] for c in city]))
 
 
 
+@csrf_exempt
+def add_university(request):
+    if request.method == "POST":
+        name = request.POST.get('name','')
+        city = request.POST.get("city", "")
+        if not name or not city:
+            return HttpResponse(json.dumps(dict(status=400)))
+        else:
+            connection = http.client.HTTPConnection("localhost", 8983)
+
+            headers = {'Content-type': 'application/json'}
+
+            foo = {'add': {'doc': {'name': name, 'city': city}, 'boost': 1, 'overwrite': True,
+                           'commitWithin': 1000}}
+            json_foo = json.dumps(foo)
+
+            connection.request('POST', '/solr/university/update?wt=json', json_foo, headers)
+
+            response = connection.getresponse()
+            response = json.loads(response.read().decode())
+            response['status'] = "success"
+            return HttpResponse(json.dumps(response))
+
+    else:
+        city = fetch_data("/solr/city/select?wt=json&indent=true&q=*:*")
+        if city['numFound'] > 0:
+            city = city['docs']
+
+            return render(request, "add_university.html", dict(city=[c['name'][0] for c in city]))
+
+
+@csrf_exempt
+def add_property(request):
+    if request.method == "POST":
+        address = request.POST.get('address','')
+        city = request.POST.get("city", "")
+        if not address or not city:
+            return HttpResponse(json.dumps(dict(status=400)))
+        else:
+            connection = http.client.HTTPConnection("localhost", 8983)
+
+            headers = {'Content-type': 'application/json'}
+
+            foo = {'add': {'doc': {'Address': address, 'city': city}, 'boost': 1, 'overwrite': True,
+                           'commitWithin': 1000}}
+            json_foo = json.dumps(foo)
+
+            connection.request('POST', '/solr/property/update?wt=json', json_foo, headers)
+
+            response = connection.getresponse()
+            response = json.loads(response.read().decode())
+            response['status'] = "success"
+            return HttpResponse(json.dumps(response))
+
+    else:
+        city = fetch_data("/solr/city/select?wt=json&indent=true&q=*:*")
+        if city['numFound'] > 0:
+            city = city['docs']
+
+            return render(request, "add_property.html", dict(city=[c['name'][0] for c in city]))
+
+
+
+@csrf_exempt
+def add_city(request):
+    if request.method == "POST":
+        city = request.POST.get("city", "")
+        if not city:
+            return HttpResponse(json.dumps(dict(status=400)))
+        else:
+            connection = http.client.HTTPConnection("localhost", 8983)
+
+            headers = {'Content-type': 'application/json'}
+
+            foo = {'add': {'doc': {'name': city}, 'boost': 1, 'overwrite': True,
+                           'commitWithin': 1000}}
+            json_foo = json.dumps(foo)
+
+            connection.request('POST', '/solr/city/update?wt=json', json_foo, headers)
+
+            response = connection.getresponse()
+            response = json.loads(response.read().decode())
+            response['status'] = "success"
+            return HttpResponse(json.dumps(response))
+
+    else:
+        return render(request, "add_city.html")
